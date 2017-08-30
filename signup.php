d44c6c9850b1884920001d2187d957b1f76dcd81
Aitor Ardila <aardila@userzoom.com>
2017-08-31 01:13:08 +0200

    FIX Name attribute

--- signup.php
+++ signup.php
@@ -2,13 +2,13 @@
 
 require 'connection.php';
 $conn = Connect();
-$name = $conn->real_escape_string($_POST['name']);
+$nombre = $conn->real_escape_string($_POST['nombre']);
 $email = $conn->real_escape_string($_POST['email']);
 $telefono = $conn->real_escape_string($_POST['telefono']);
 $poblacion = $conn->real_escape_string($_POST['poblacion']);
 $idiomasHablados = $conn->real_escape_string($_POST['idiomasHablados']);
 $idiomasAprendes = $conn->real_escape_string($_POST['idiomasAprendes']);
-$query   = "INSERT into form_users (name,email,phone,city,languageSpoken,languageLearnt) VALUES('" . $name . "','" . $email . "',
+$query   = "INSERT into form_users (name,email,phone,city,languageSpoken,languageLearnt) VALUES('" . $nombre . "','" . $email . "',
   '" . $telefono . "','" . $poblacion . "','" . $idiomasHablados . "','" . $idiomasAprendes . "')";
 $success = $conn->query($query);
 
