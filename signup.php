93b41f13b920976d5b4f08f6fe39edd1a4f74b1e
Aitor Ardila <aardila@userzoom.com>
2017-08-31 13:01:13 +0200

    Only INSERT new emails

--- signup.php
+++ signup.php
@@ -8,15 +8,22 @@ $telefono = $conn->real_escape_string($_POST['telefono']);
 $poblacion = $conn->real_escape_string($_POST['poblacion']);
 $idiomasHablados = $conn->real_escape_string($_POST['idiomasHablados']);
 $idiomasAprendes = $conn->real_escape_string($_POST['idiomasAprendes']);
-$query   = "INSERT into form_users (name,email,phone,city,languageSpoken,languageLearnt) VALUES('" . $nombre . "','" . $email . "',
-  '" . $telefono . "','" . $poblacion . "','" . $idiomasHablados . "','" . $idiomasAprendes . "')";
-$success = $conn->query($query);
+$query0 = "SELECT count(1) FROM form_users WHERE email = '" . $email . "'";
+$result = $conn->query($query0);
+$values = $result->fetch_array(MYSQLI_NUM);
+$num_rows = $values[0];
 
-if (!$success) {
-    die("Couldn't enter data: ".$conn->error);
+if($num_rows < 1) {
+  $query   = "INSERT into form_users (name,email,phone,city,languageSpoken,languageLearnt) VALUES('" . $nombre . "','" . $email . "',
+    '" . $telefono . "','" . $poblacion . "','" . $idiomasHablados . "','" . $idiomasAprendes . "')";
+  $success = $conn->query($query);
 
+  if (!$success) {
+      die("Couldn't enter data: ".$conn->error);
+
+  }
+  $conn->close();
 }
-echo $success;
-$conn->close();
+echo 1;
 
 ?>
